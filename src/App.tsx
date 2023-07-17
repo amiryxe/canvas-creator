540b1b52998f43ef7c1fcce0019544718c5e25e9
Amir Salehi <nik.amirsa@gmail.com>
2023-07-17 19:05:04 +0330

    install html2canvas and use it to create canvas from text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import { useRef, useState } from 'react'
+import html2canvas from 'html2canvas'
+import './assets/styles/main.scss'
+
 export default function App() {
+  const [content, setContent] = useState('')
+  const contentRef = useRef<HTMLElement>()
+
+  const submitHandler = (e: any) => {
+    e.preventDefault()
+
+    setContent(e.target.content.value)
+  }
+
+  const buildHandler = () => {
+    console.log(contentRef.current)
+
+    if (contentRef.current) {
+      html2canvas(contentRef.current).then(function (canvas) {
+        document.body.appendChild(canvas);
+      })
+    }
+  }
 
   return (
     <>
-      hello
+      <form onSubmit={submitHandler}>
+        <input name='content' />
+
+        <button type='submit'>
+          create
+        </button>
+      </form>
+
+      <div ref={contentRef} className='content'>
+        {content}
+      </div>
+
+      <button onClick={buildHandler}>Build image</button>
     </>
   )
 }
